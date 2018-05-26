508700c1cb374c29e73cee95046cf632726da72e
Nikhil VJ <answerquest@users.noreply.github.com>
2018-05-26 22:18:28 +0530

    files

---
+++ config.js
@@ -0,0 +1,5 @@
+tilePath = 'tiles';
+
+maxZoom = 5;
+
+attributionHTML = 'Wikimedia Commons: <a href="https://commons.wikimedia.org/wiki/Template:Radha_and_Krishna_in_the_boat_of_love_-_Google_Art_Project">Radha and Krishna in the boat of love</a>, National Museum, New Delhi, India ';
